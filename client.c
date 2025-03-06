ff13b85be500dfd8a662f7b70effc948d97b38a4
Edgar X. Gonzalez Cortes <exgc12@gmail.com>
2025-03-06 01:07:28 -0500

    save progress

--- client.c
+++ client.c
@@ -3,62 +3,81 @@
 #include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
+#include <sys/select.h>
 
 #define PORT 8080
 
-int main() {
-  int sock = 0;
-  struct sockaddr_in serv_addr;
+#define RESET_COLOR "\033[0m"
+#define CYAN "\033[96m"
+#define YELLOW "\033[93m"
+#define GREEN "\033[92m"
+
+int main()
+{
+  int sock;
+  struct sockaddr_in server_address;
   char buffer[1024] = {0};
 
-  // Create socket
   sock = socket(AF_INET, SOCK_STREAM, 0);
-  if (sock < 0) {
+  if (sock < 0)
+  {
     perror("Socket creation error");
-    return -1;
+    exit(EXIT_FAILURE);
   }
 
-  serv_addr.sin_family = AF_INET;
-  serv_addr.sin_port = htons(PORT);
+  server_address.sin_family = AF_INET;
+  server_address.sin_port = htons(PORT);
+  inet_pton(AF_INET, "127.0.0.1", &server_address.sin_addr);
 
-  // Convert address and connect
-  if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
-    perror("Invalid address / Address not supported");
-    return -1;
+  if (connect(sock, (struct sockaddr *)&server_address, sizeof(server_address)) < 0)
+  {
+    perror("Connection Failed");
+    exit(EXIT_FAILURE);
   }
 
-  if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
-    perror("Connection failed");
-    return -1;
-  }
+  printf(GREEN "Connected to server. Type 'exit' to quit.\n" RESET_COLOR);
 
-  printf("Connected to server. Type 'exit' to quit.\n");
+  fd_set read_fds;
+  while (1)
+  {
+    printf(CYAN "You: " RESET_COLOR);
+    fflush(stdout);
 
-  while (1) {
-    // Get user input
-    printf("Client: ");
-    fgets(buffer, sizeof(buffer), stdin);
+    FD_ZERO(&read_fds);
+    FD_SET(STDIN_FILENO, &read_fds);
+    FD_SET(sock, &read_fds);
 
-    // Send message
-    send(sock, buffer, strlen(buffer), 0);
+    select(sock + 1, &read_fds, NULL, NULL, NULL);
 
-    // Exit if user types "exit"
-    if (strncmp(buffer, "exit", 4) == 0) {
-      printf("Closing connection.\n");
-      break;
+    if (FD_ISSET(STDIN_FILENO, &read_fds))
+    {
+      memset(buffer, 0, sizeof(buffer));
+      fgets(buffer, sizeof(buffer), stdin);
+      send(sock, buffer, strlen(buffer), 0);
+
+      if (strncmp(buffer, "exit", 4) == 0)
+      {
+        printf(GREEN "Closing connection.\n" RESET_COLOR);
+        break;
+      }
     }
 
-    // Receive response
-    memset(buffer, 0, sizeof(buffer));
-    int valread = read(sock, buffer, sizeof(buffer));
-    if (valread > 0) {
-      printf("Server: %s", buffer);
-    }
+    if (FD_ISSET(sock, &read_fds))
+    {
+      memset(buffer, 0, sizeof(buffer));
+      int valread = read(sock, buffer, sizeof(buffer));
+      if (valread > 0)
+      {
+        printf("\n" YELLOW "Server: %s" RESET_COLOR, buffer);
+        printf(CYAN "You: " RESET_COLOR);
+        fflush(stdout);
+      }
 
-    // Exit if server sends "exit"
-    if (strncmp(buffer, "exit", 4) == 0) {
-      printf("Server closed the connection.\n");
-      break;
+      if (strncmp(buffer, "exit", 4) == 0)
+      {
+        printf(GREEN "\nServer closed the connection.\n" RESET_COLOR);
+        break;
+      }
     }
   }
 
--- server.c
+++ server.c
@@ -3,78 +3,85 @@
 #include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
+#include <sys/select.h>
 
 #define PORT 8080
 
-int main() {
+#define RESET_COLOR "\033[0m"
+#define CYAN "\033[96m"
+#define YELLOW "\033[93m"
+#define GREEN "\033[92m"
+
+int main()
+{
   int server_fd, new_socket;
   struct sockaddr_in address;
   int addrlen = sizeof(address);
   char buffer[1024] = {0};
 
-  // Create socket
   server_fd = socket(AF_INET, SOCK_STREAM, 0);
-  
-  if (server_fd == 0) {
+  if (server_fd == 0)
+  {
     perror("Socket failed");
     exit(EXIT_FAILURE);
   }
 
-  // Bind to port
   address.sin_family = AF_INET;
-  address.sin_addr.s_addr = INADDR_ANY; // Accept connections on any interface
+  address.sin_addr.s_addr = INADDR_ANY;
   address.sin_port = htons(PORT);
+  bind(server_fd, (struct sockaddr *)&address, sizeof(address));
 
-  if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
-    perror("Bind failed");
-    exit(EXIT_FAILURE);
-  }
+  listen(server_fd, 3);
+  printf(GREEN "Server listening on port %d...\n" RESET_COLOR, PORT);
 
-  // Start listening
-  if (listen(server_fd, 3) < 0) {
-    perror("Listen failed");
-    exit(EXIT_FAILURE);
-  }
-
-  printf("Server listening on port %d...\n", PORT);
-
-  // Accept a connection
   new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen);
-  if (new_socket < 0) {
-    perror("Accept failed");
-    exit(EXIT_FAILURE);
-  }
+  printf(GREEN "Client connected.\n" RESET_COLOR);
 
-  while(1) {
-    // Receive message
-    memset(buffer, 0, sizeof(buffer));
-    int valread = read(new_socket, buffer, sizeof(buffer));
-    if (valread > 0) {
-      printf("Client: %s", buffer);
+  fd_set read_fds;
+  while (1)
+  {
+    printf(CYAN "You: " RESET_COLOR);
+    fflush(stdout);
+
+    FD_ZERO(&read_fds);
+    FD_SET(STDIN_FILENO, &read_fds);
+    FD_SET(new_socket, &read_fds);
+
+    select(new_socket + 1, &read_fds, NULL, NULL, NULL);
+
+    if (FD_ISSET(STDIN_FILENO, &read_fds))
+    {
+      memset(buffer, 0, sizeof(buffer));
+      fgets(buffer, sizeof(buffer), stdin);
+      send(new_socket, buffer, strlen(buffer), 0);
+
+      if (strncmp(buffer, "exit", 4) == 0)
+      {
+        printf(GREEN "Closing connection.\n" RESET_COLOR);
+        break;
+      }
     }
 
-    // Check for exit condition
-    if (strncmp(buffer, "exit", 4) == 0) {
-      printf("Client disconnected.\n");
-      break;
-    }
+    if (FD_ISSET(new_socket, &read_fds))
+    {
+      memset(buffer, 0, sizeof(buffer));
+      int valread = read(new_socket, buffer, sizeof(buffer));
+      if (valread > 0)
+      {
+        printf("\n" YELLOW "Client: %s" RESET_COLOR, buffer);
+        printf(CYAN "You: " RESET_COLOR);
+        fflush(stdout);
+      }
 
-    // Get user input
-    printf("Server: ");
-    fgets(buffer, sizeof(buffer), stdin);
-
-    // Send message
-    send(new_socket, buffer, strlen(buffer), 0);
-
-    // Exit if user types "exit"
-    if (strncmp(buffer, "exit", 4) == 0) {
-      printf("Closing connection.\n");
-      break;
+      if (strncmp(buffer, "exit", 4) == 0)
+      {
+        printf(GREEN "\nClient disconnected.\n" RESET_COLOR);
+        break;
+      }
     }
   }
 
   close(new_socket);
   close(server_fd);
-
   return 0;
 }
